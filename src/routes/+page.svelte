4a322c83aab94d10357864edc4913f5136cb1a1c
loke-dev <lokeinbox@gmail.com>
2026-03-15 10:00:00 +0100

    feat: add TechStackSection with badge row

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import HeroSection from '$components/sections/HeroSection.svelte';
 	import FeaturesSection from '$components/sections/FeaturesSection.svelte';
+	import TechStackSection from '$components/sections/TechStackSection.svelte';
 </script>
 
 <HeroSection />
 <FeaturesSection />
+<TechStackSection />
